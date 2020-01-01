c385d8a09d14ace121b047efb5876644e839d848
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-01-01 15:17:32 +0900

    신구약 나누기

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 $(function(){
-    $("input.ge").change(function(){
-        if($("input.ge").is(":checked")){
-            $("i.fa-circle").addClass("fa-dot-circle");
-            $("i.fa-circle").removeClass("fa-circle");
-        } else {
-            $("i.fa-dot-circle").addClass("fa-circle");
-            $("i.fa-dot-circle").removeClass("fa-dot-circle");
-        }
-    })
+
 });
+function goPage(page) {
+    location.href = page;
+    return;
+}
